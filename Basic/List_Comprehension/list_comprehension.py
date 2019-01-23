d4d9c9caffdc04d7b681add86d049dfe5f290d1f
kundan kumar <kkundan7kumar@gmail.com>
2019-01-23 10:36:08 -0600

    code

---
+++ Basic/List_Comprehension/list_comprehension.py
@@ -0,0 +1,20 @@
+# List and it's iteration
+# Add element in the list
+a=[]
+for i in range(1,11):
+    a.append(i)
+
+print(a)
+
+# With the list comprehension
+
+b=[j for j in range(1,11)]
+print(b)
+
+#adding same element in the list
+
+c=[1]*10
+print(c)
+
+
+
