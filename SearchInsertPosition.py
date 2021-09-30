496997bf017cb3335cdf1feca955857be80ec92c
kundan kumar <kkundan7kumar@gmail.com>
2021-09-30 09:47:08 -0500

    Update SearchInsertPosition.py

--- SearchInsertPosition.py
+++ SearchInsertPosition.py
@@ -2,6 +2,11 @@
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
+
+        return len([x for x in nums if x<target])
+        class Solution:
+    def searchInsert(self, nums, target):
+        return bisect.bisect_left(nums, target)
 """
 
 class Solution:
